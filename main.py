a0e34468cf54ebc23a224b09ba2eb2101e023e59
Ryan Augustyn <jraugustyn07@gmail.com>
2023-02-12 16:36:44 -0600

    first working version

--- main.py
+++ main.py
@@ -40,20 +40,21 @@ def user_feedback(user_list):
         user_feedback = int(input("Choose what to change:\n1 for Destination\n2 for Restaurant\n3 for Transportation\n4 for Activity\n5 for everything\n"))
         return user_feedback
         
-
 #return new item(s) based on user feeback 
 def get_new_item(user_feedback, user_list):
-    if user_feedback == 5:  
-        #rerun initial setup
-        print(trip_choices(all_lists))
-    elif user_feedback >= 1 or user_feedback < 5:
-        index = user_feedback - 1
-        list_to_change = all_lists[index]
-        unwanted_item = user_list[index]
+    index = user_feedback - 1
+    list_to_change = all_lists[index]
+    unwanted_item = user_list[index]
+    new_item = random.choice(list_to_change)
+    while new_item == unwanted_item:
         new_item = random.choice(list_to_change)
-        while new_item == unwanted_item:
-            new_item = random.choice(list_to_change)
-        return new_item
+    return new_item
+
+#start over with new list
+def get_new_list(user_list):
+    new_list = trip_choices(all_lists) 
+    readout_of_options(new_list)
+    return new_list
 
 #add in change from get_new_item function    
 def revise_list(user_list, user_change, new_item):
@@ -61,21 +62,29 @@ def revise_list(user_list, user_change, new_item):
     user_list[index] = new_item
     return user_list
 
-
+#program to run everything else
 def run_program(all_lists):
     #initial run
     user_list = trip_choices(all_lists)
     readout_of_options(user_list)
     #feedback
     feedback = user_feedback(user_list)
-    if feedback != 0:
-        new_item = get_new_item(feedback, user_list)
-        user_list = revise_list(user_list, feedback, new_item)
-        print(user_list)
+    while(feedback != 0):
+        if feedback >= 1 and feedback < 5:
+            new_item = get_new_item(feedback, user_list)
+            user_list = revise_list(user_list, feedback, new_item)
+            readout_of_options(user_list)
+        elif feedback == 5:
+            user_list = get_new_list(user_list)  
+
+        feedback = user_feedback(user_list)
 
 
 ###START PROGRAM###
 
 run_program(all_lists)
+
+
+#thought: add in list of choices that can be looped through to check if picked options 1-4
 
 
